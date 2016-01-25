753d2b44b0cae905eaaadddf18f01009a0a42666
Wesley Castro <wes@Wesleys-MacBook-Pro.local>
2016-01-24 22:00:23 -0800

    Add previous key. Cleanup.

--- background.js
+++ background.js
@@ -2,8 +2,6 @@ var tabid = -1;
 
 // Forward command events to content script
 chrome.commands.onCommand.addListener(function(command) {
-    console.log("Forwarding command: "+command);
-    //chrome.runtime.sendMessage({cmd: command});
     chrome.tabs.sendMessage(tabid, {cmd: command});
 });
 
